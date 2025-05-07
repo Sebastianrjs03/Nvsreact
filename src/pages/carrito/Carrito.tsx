d57b1a2c0a4de91d6fc8b4105d648c125b098340
Iku2326 <nicolasrami2006@gmail.com>
2025-05-07 11:11:50 -0500

    carrito-componentes-inicio

---
+++ src/pages/carrito/Carrito.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+export function Carrito() {
+  return (
+
+    <React.Fragment>
+
+        <main className="carrito-main">
+
+            
+        </main>
+
+    </React.Fragment>
+
+  );
+}
+
+export default Carrito;
