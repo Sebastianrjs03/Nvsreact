3d06c793bac8e62657e0badcebe517bd5e0c2098
Karen-alvarez04 <Karen-alvarez04>
2025-05-08 13:53:06 -0500

    Empiezo api

--- api-php/models/producto.php
+++ api-php/models/producto.php
@@ -11,22 +11,65 @@ class Producto
         $this->conn = Database::conectar();
     }
 
-    public function obtenerProductos($plataforma, $tipoProducto, $categoria)
+    public function obtenerProductos($plataforma, $tipoProducto, $genero)
     {
-        if ($tipoProducto == "") {
-            if ($plataforma && $categoria) {
+        if ($tipoProducto == "Videojuego") {
+            if ($plataforma && $genero) {
+              
+            $placeholder = "Hola";    
+
+
+             $sql = "
+             SELECT 
+             p.idProducto,
+             p.precioProducto,
+             p.nombreProducto,
+             pl.idPlataforma AS plataforma,
+             ge.idGeneroJuego AS generojuego
+             FROM producto p
+             JOIN juego j ON p.idProducto = j.idJuego
+             JOIN aux_plataforma ap ON j.idJuego = ap.idJuego
+             JOIN plataforma pl ON ap.idPlataforma = pl.idPlataforma
+             JOIN aux_genero ag ON j.idJuego = ag.fk_pk_juego
+             JOIN generojuego ge ON ag.fk_pk_genero = ge.idGeneroJuego
+             WHERE pl.idPlataforma IN ($placeholder)
+             AND ge.idGeneroJuego = :genero
+             AND p.stock > 0
+             
+             ";
 
             } elseif ($plataforma) {
 
-            } elseif ($categoria) {
+                $sql = "
+            SELECT 
+             p.idProducto,
+             p.precioProducto,
+             p.nombreProducto,
+             pl.idPlataforma AS plataforma
+             FROM producto p
+             JOIN juego j ON p.idProducto = j.idJuego
+             JOIN aux_plataforma ap ON j.idJuego = ap.idJuego
+             JOIN plataforma pl ON ap.idPlataforma = pl.idPlataforma
+             WHERE pl.idPlataforma = :plataforma
+             AND p.stock > 0
+            ";
 
+            $stmt = $this->conn->prepare($sql);
+            $stmt-> bindParam(":plataforma", $plataforma, PDO::PARAM_STR);
+
+            } elseif ($genero) {
+                
             } else {
-
+                $mensaje = ("Hola") ? $sql = "": $sql = "";
             }
 
         } else {
 
         }
+
     }
+
+
+ 
 }
 ?>
