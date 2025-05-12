40f88a0a73f0ce017196c57b825b15d00c7f9516
AndreyRB1 <darb.styl3@gmail.com>
2025-05-11 23:40:43 -0500

    formularios login, responsive, hechos componentes y organizados

---
+++ src/components/login/logo.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import logo from '../../assets/login/n_blanca.svg';
+
+const Logo = () => {
+  return <img src={logo} alt="Logo NVS" width="20%" />;
+};
+
+export default Logo;
