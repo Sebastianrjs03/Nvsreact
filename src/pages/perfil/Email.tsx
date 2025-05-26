1da9a6068d09b937e8a9c3358ede87d36b40bd8a
Sebastian Rojas <ysrojas87@soy.sena.edu.co>
2025-05-26 02:56:47 -0500

    email ruta

--- src/pages/perfil/Email.tsx
+++ src/pages/perfil/Email.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "../../styles/Perfil/Datos.css";
-import FormEditar from "../../components/Editar/formEditar";
+import FormEditar from "../../components/Editar/FormEditar";
 
 function Email() {
   return (
